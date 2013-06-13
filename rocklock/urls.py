3c0a295557f15952de95c20a7ae5b358f132a886
13oz <duminsky.nick@gmail.com>
2013-06-13 17:16:46 +0400

    urls commit
    changed home page from newsline to blog

--- rocklock/urls.py
+++ rocklock/urls.py
@@ -7,9 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'rocklock.views.home', name='home'),
-    url(r'^', include('rocklock.newsline.urls')),
-
-    url(r'^blog/', include('rocklock.blog.urls')),
+    url(r'^', include('rocklock.blog.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
